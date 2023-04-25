fd3f1278a72cbd09dadf28ca30cf8db6e8fdf888
ventureful <hades0122@outlook.com>
2023-04-25 21:24:27 +0800

    fix h2-console access despite security

--- src/main/java/com/example/employee/config/SecurityConfiguration.java
+++ src/main/java/com/example/employee/config/SecurityConfiguration.java
@@ -17,19 +17,29 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfiguration {
+    private final static String H2_PATH = "/h2-console/**";
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http, JwtAuthenticationExceptionHandling jwtAuthenticationExceptionHandling) throws Exception {
         http.authorizeHttpRequests(authorize -> authorize
                 .requestMatchers("/api/v1/auth/**").permitAll()
+                .requestMatchers(new AntPathRequestMatcher(H2_PATH)).permitAll()
                 .anyRequest().authenticated());
 
         http.authenticationProvider(authenticationProvider());
         http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
-        http.cors().and().csrf().disable();
+        http.cors().disable();
+
+        // to exempt /h2-console from security
+        http.headers().frameOptions().disable();
+        http.csrf(csrf -> csrf
+                .ignoringRequestMatchers(AntPathRequestMatcher.antMatcher(H2_PATH)));
+
         http.exceptionHandling().authenticationEntryPoint(jwtAuthenticationExceptionHandling);
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
